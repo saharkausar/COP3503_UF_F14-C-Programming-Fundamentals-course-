af7aa8c07245ca96031452bbd0bd7ef59376fb91
Sahar Kausar <saharkausar@gmail.com>
2016-11-20 10:46:49 -0500

    Update pa1.cpp

--- pa1.cpp
+++ pa1.cpp
@@ -5,6 +5,14 @@
  Section: 8228
  */
 
+/**
+
+This C++ program creates an N*N magic square, 
+i.e., a square arrangement of the numbers 1, 2, , , in which the sum of rows, columns, and diagonals are the same. 
+The user will specify the size of the square matrix: N. The value N must be an odd number between 3 and 15.
+
+**/
+
 #include <iostream>
 
 using namespace std;
